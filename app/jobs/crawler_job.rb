05f6b0b2681d54ebb3a7693144551c53738938a3
Bryan White <bryanchriswhite@gmail.com>
2016-05-23 17:39:52 +0200

    trying string keys cos redis

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -1,7 +1,7 @@
 class CrawlerJob < Batch::Job
   def run(product_hash)
-    @product_url = product_hash[:product_url]
-    provider_name = product_hash[:provider_name]
+    @product_url = product_hash['product_url']
+    provider_name = product_hash['provider_name']
     provider = Crawler::Provider.lookup(name: provider_name)
     result = provider.new(@product_url).scrape
     Crawler::RedisStorage.new(@batch_id).push(@jid, result)
