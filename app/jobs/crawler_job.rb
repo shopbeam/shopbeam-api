f1709660e46fc6e0143fc4524840fb1450be20f7
Bryan White <bryanchriswhite@gmail.com>
2016-05-23 17:14:30 +0200

    bugfixing

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -2,7 +2,7 @@ class CrawlerJob < Batch::Job
   def run(product_url:, provider_name:)
     @product_url = product_url
     provider = Crawler::Provider.lookup(name: provider_name)
-    result = provider.new(product_hash).scrape
+    result = provider.new(product_url).scrape
     Crawler::RedisStorage.new(@batch_id).push(@jid, result)
   end
 
